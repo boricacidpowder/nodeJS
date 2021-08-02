d123c17986f4c11d8505ad1d746859440bb7c9b3
룰루랄라 <tmddyd0866@naver.com>
2021-08-02 13:28:15 +0000

    cookie read

--- cookie.js
+++ cookie.js
@@ -1,7 +1,14 @@
 var http = require('http');
+var cookie = require('cookie');
+
 http.createServer(function(req, res){
-	// res.writeHead(200, {
-	// 	'Set-Cookie':['yum_cookie=choco', 'tes_cookie=str']
-	// });
-	// res.end('Cookie!');
+	var cookies = {};
+	if(req.headers.cookie !== undefined){
+		cookies = cookie.parse(req.headers.cookie);
+	}
+	console.log(cookies.yum_cookie);
+	res.writeHead(200, {
+		'Set-Cookie':['yum_cookie=choco', 'tes_cookie=str']
+	});
+	res.end('Cookie!');
 }).listen(3000);
