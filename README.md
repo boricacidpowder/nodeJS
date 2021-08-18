17aae36c5218c610c37feb01dce945940e602d21
룰루랄라 <tmddyd0866@naver.com>
2021-08-18 14:15:18 +0000

    mongodb..

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # nodeJS
 My first nodeJS container
+
+
+1. MongoDB 설치
+
+npm install mongodb --save
+
+3. MongoDB 서버 설치
+
+sudo apt-get install mongodb-server
+ 
+
+4. MongoDB 설정 및 시작
+
+sudo service mongodb start
+sudo service mongodb stop
+sudo service mongodb restart
+ 
+
+5. MongoDB 쉘 설정
+
+mongod
+ 
+
+6. MongoDB 쉘 시작
+
+mongo
+ 
+
+7. MongoDB 데이터베이스 선택(생성)
+
+use local
+ 
+
+8. MongoDB 데이터베이스 조회
+
+show DBs
+ 
+
+9. MongoDB 테이블 레코드 조회
+
+DB.table.find()
--- app.js
+++ app.js
@@ -1,105 +1,52 @@
-const express = require('express'),
-	path = require('path'),
-	morgan = require('morgan'),
-	cookieParser = require('cookie-parser'),
-	session = require('express-session'),
-	dotenv = require('dotenv'),
-	bodyParser = require('body-parser'),
-	multer = require('multer'),
-	fs = require('fs');
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const path = require('path');
+const nunjucks = require('nunjucks');
 
+const connect = require('./mongo');
 
 dotenv.config();
-const indexRouter = require('./routes'),
-	userRouter = require('./routes/user');
+const indexRouter = require('./routes');
+const userRouter = require('./routes/user');
+const commentsRouter = require('./routes/comments');
+
 const app = express();
 app.set('port', process.env.PORT || 3000);
-//pug 사용
-app.set('views', path.join(__dirname, 'views'));
-app.set('views engine', 'pug');
+app.set('view engine', 'html');
+nunjucks.configure('view', {
+	express: app,
+	watch: true,
+});
+connect();
 
 app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(bodyParser.raw());
-app.use(bodyParser.text());
-app.use(session({
-	resave: false,
-	saveUninitialized: false,
-	sevret: process.env.COOKIE_SECRET,
-	cookie: {
-		httpOnly: true,
-		secure: false,
-	},
-	name: 'session-cookie',
-}));
+app.use(express.urlencoded({ extended: false }));
 
-//라우터 설정
+
 app.use('/', indexRouter);
 app.use('/user', userRouter);
+app.use('/comments', commentsRouter);
 
 app.use((req, res, next) => {
-	res.status(404).send('Not Found');
+	const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+	console.log(req.method);
+	error.status = 404;
+	next(error);
 });
 
 app.use((err, req, res, next) => {
-	console.log(err)
-	res.status(500).send(err.message);
-});
-
-try {
-	fs.readdirSync('upload');
-} catch (error) {
-	console.error('upload 폴더가 없어서 생성합니다.');
-	fs.mkdirSync('upload');
-}
-
-const upload = multer({
-	storage: multer.diskStorage({
-		destination(req, file, done) {
-			done(null, 'upload/');
-		},
-		filename(req, file, done){
-			const ext = path.extname(file.originalname);
-			done(null, path.basename(file.originalname, ext) + Data.now() + ext);
-		},
-	}),
-	limits: { fileSize: 5 * 1024 * 1024},
-});
-
-app.post('/upload', upload.array('image'), (req, res) => {
-	console.log(req.file, req.body)
-	res.send('ok');
-})
-app.post('/upload',
-		upload.fields([{ name: 'image1'} , { name: 'image2'}]),
-		(req, res) => {
-			console.log(req.filesm, req.body);
-			res.send('ok');
-		},
-	);
-
-
-// 404 처리 미들웨어
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// 에러 핸들러
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	res.locals.message = err.message;
+	res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+	console.log(process.env.NODE_ENV);
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 app.listen(app.get('port'), () => {
-	console.log(app.get('port'), '에서 대기중');
+	console.log(app.get('port'), '번 포트에서 대기 중');
 });
-
-module.exports = app;
